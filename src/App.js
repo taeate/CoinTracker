49cafef313c543004b5fef7e09b7fac09f1f2087
taeate <yes3144@nate.com>
2022-11-26 21:08:01 +0900

    coin api 를 가져와서 간단하게 보여주기

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import './App.css';
+
+function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, [])
+  return <div>
+    <h1>The Coins!</h1>
+    {loading ? <strong>Loading...</strong> : null}
+    <ul>
+      {coins.map((coin) => <li>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</li>)}
+    </ul>
+  </div>;
+}
+
+export default App;
